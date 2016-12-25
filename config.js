4939120ddfdb6c0c6b8c7ea2fbd2116524cd4043
Andoni Sánchez <andoni.sanchez.antolin@gmail.com>
2016-12-25 18:03:32 +0100

    mysql connection

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.mysql = {};
+
+config.mysql.host = 'sql8.freesqldatabase.com';
+config.mysql.user = 'sql8150912';
+config.mysql.password = 'Ang78Tn9tz';
+config.mysql.database = 'sql8150912';
+
+module.exports = config;
