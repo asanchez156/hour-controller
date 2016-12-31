8dd3c3a9bf864631e9597ffd54a6d47f6f3dbd5f
Andoni Sánchez <andoni.sanchez.antolin@gmail.com>
2016-12-31 17:13:20 +0100

    Hour controller created

---
+++ controllers/hourController.js
@@ -0,0 +1,43 @@
+// controllers/hourController.js
+
+var models = require("../models/models.js")
+
+exports.index = function(req, res, next) {
+	res.render('hour', {
+	    	title: 'Jornadas',
+			parentPage : 'Pantalla principal',
+			page: 'hour',
+  			user: req.session.user
+	    });
+  	/*models.Workingday.findAll().then(function(quizes) {
+  		res.render('hour', {
+	    	title: 'Jornadas',
+			parentPage : 'Pantalla principal',
+			page: 'hour',
+  			user: req.session.user,
+  			workingdays: "data"
+	    });
+  }).catch(function(error) {
+    next(error);
+  });*/
+}
+
+exports.create = function(req, res, next) {
+   console.log("Create Workingday");
+   /*
+  var search = ("%" + (req.query.search || "") + "%").replace(' ', '%');
+  	models.Workingday.findAll().then(function(quizes) {
+	    res.render('hour', {
+	    	title: 'Jornadas',
+			parentPage : 'Pantalla principal',
+			page: 'hour'
+	      	// devuelve una lista ordenada si se ha realizado una busqueda
+	      	hours: (req.query.search) ? quizes.sort(function(a, b) {
+	        	return a.pregunta > b.pregunta;
+	      	}) : quizes,
+	      errors: [],
+	    })
+  }).catch(function(error) {
+    next(error);
+  });*/
+}
