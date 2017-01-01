0ba06d32a4037e368cef27c6b71485e1bb98f0ca
Andoni Sánchez <andoni.sanchez.antolin@gmail.com>
2017-01-01 14:07:36 +0100

    Hour use case

--- public/assets/js/hour-table.js
+++ public/assets/js/hour-table.js
@@ -43,3 +43,52 @@ function searchWorkingday(){
 function newWorkingdayForm(){
 
 }
+
+function newWorkingday(){
+	var user = "'"+<%= user.username %>+"'";
+	var date = $('#date').val();
+	var endDate = $('#endDate').val();
+
+	var workingdayDataList = [];
+	$('#newworkingDayPanel').forEach(function(value, index){
+		var workingdayData = {
+			employee : $('#employee'+index).val(),
+			date : $('#employee'+index).val(),
+		}
+	});
+
+
+	workingdayDataList.push(workingdayData)
+
+	$.post('/hour/create', workingdayData , function(data) {
+		
+	})
+	.done(function() {
+	    //alert( "second success" );
+	})
+	.fail(function() {
+	    //alert( "error" );
+	})
+	.always(function() {
+	    //alert( "finished" );
+	});
+}
+
+function newPale(){
+	var paleData = {
+		
+	}
+
+	$.post('/pale/create', paleData , function(data) {
+		
+	})
+	.done(function() {
+	    //alert( "second success" );
+	})
+	.fail(function() {
+	    //alert( "error" );
+	})
+	.always(function() {
+	    //alert( "finished" );
+	});
+}
