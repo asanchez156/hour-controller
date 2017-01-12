8272de34f812baa88aaaa493ce74aff37216f150
Andoni <asanchez@indaba.es>
2017-01-12 13:57:32 +0100

    reestructured aplication routes for login

--- routes/index.js
+++ routes/index.js
@@ -5,58 +5,51 @@ var router = express.Router();
 var sessionController = require('../controllers/sessionController');
 var hourController = require('../controllers/hourController');
 
-var params = function (title, parentPage, page, user, status) { 
+var params = function (title, parentPage, page, user, messages) { 
     return {
               title: title,
               parentPage : parentPage,
               page: page ,
               user: user,
-              status: status
+              messages: messages
     }
 }
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', sessionController.messages, function(req, res) {
     res.render('home', { title: 'Pantalla principal',
   						page: 'home' ,
-  						user: req.session.user });
+  						user: req.session.user,
+              messages: req.session.messages,
+              login : 0});
 });
 
 /* GET Login. */
-router.get('/login', function(req, res) {
-    res.render('login', params('Iniciar sesión','Pantalla principal','login', req.session.user, undefined));
-});
-
-/* GET Login error. */
-router.get('/login/error', function(req, res) {
-    res.render('login', params('Iniciar sesión','Pantalla principal','login', req.session.user, {error : req.session.error, success : undefined}));
-});
-
-/* GET Page page. */
-router.get('/page', function(req, res) {
-  res.render('page', params('Página','Pantalla principal','page', req.session.user, undefined));
-});
-
-/* GET about page. */
-router.get('/about', function(req, res) {
-  res.render('about', params('Saber más','Pantalla principal','about', req.session.user, undefined));
+router.get('/login', sessionController.messages, function(req, res) {
+    res.render('home', { title: 'Pantalla principal',
+              page: 'home' ,
+              user: req.session.user,
+              messages: req.session.messages,
+              login: 1});
 });
 
 /* Authentication */
 router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
 
+/* GET Page page. */
+router.get('/page', sessionController.loginRequired, sessionController.messages, function(req, res) {
+  res.render('page', params('Página','Pantalla principal','page', req.session.user, req.session.messages));
+});
+
+/* GET about page. */
+router.get('/about', sessionController.messages, function(req, res) {
+  res.render('about', params('Saber más','Pantalla principal','about', req.session.user, req.session.messages));
+});
+
 /* Hour */
-
-router.get('/hour', sessionController.loginRequired, hourController.index);
-router.post('/hour', sessionController.loginRequired, hourController.find);
-router.post('/hour/create', sessionController.loginRequired, hourController.create);
-router.get('/hour/error', function(req, res) {
-    res.render('hour', params('Jornada','Pantalla principal','hour', req.session.user, req.session.error, {error : req.session.error, success : {}}));
-});
-
-router.get('/*', function(req, res) {
-	res.redirect("/");
-});
+router.get('/hour', sessionController.loginRequired, sessionController.messages, hourController.index);
+router.post('/hour', sessionController.loginRequired, sessionController.messages, hourController.find);
+router.post('/hour/create', sessionController.loginRequired, sessionController.messages, hourController.create);
 
 module.exports = router;
