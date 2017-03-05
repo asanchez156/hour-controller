e58b623b83ce64fa236847d8894ff294171ab140
Andoni <andoni.sanchez.antolin@gmail.com>
2017-03-05 10:52:04 +0100

    changed upload size limit

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.set('view engine', 'html');
 
 app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 app.use(cookieParser('hour-controller-2017'));
 app.use(express.static(path.join(__dirname, 'public')));
 
