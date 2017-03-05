be4e7a6309a6f5fef04c69255491be0430b78b82
Andoni <andoni.sanchez.antolin@gmail.com>
2017-03-05 10:56:49 +0100

    the model still has an error

--- models/models.js
+++ models/models.js
@@ -100,7 +100,8 @@ Company.hasMany(Pale,{
 
 WorkingDay.belongsTo(User, { foreignKey: 'userId'});
 WorkingDay.belongsTo(Employee, { foreignKey: 'employeeId'});
-Employee.belongsTo(User, { foreignKey: 'employeeId' }); // ESTA COGIENDO MAL EL EMPLEADO
+Employee.belongsTo(User, { foreignKey: 'employeeId' });
+// ESTA COGIENDO MAL EL EMPLEADO 
 //Employee.belongsTo(Company, { foreignKey: 'companyId' });
 Employee.belongsToMany(Company, { through: EmployeeCompany, foreignKey: 'employeeId' });
 Company.belongsToMany(Employee, { through: EmployeeCompany, foreignKey: 'companyId' });
